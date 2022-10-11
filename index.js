acaab8f4df97fddce91a5263783629767dbd2478
MiguelGarcia98Github <miguelgarciard98@gmail.com>
2022-10-11 21:40:31 +0200

    Fixed strictequals file: all cases working with Object.is

---
+++ index.js
@@ -0,0 +1,14 @@
+import { strictEquals } from "./strictEquals.js";
+
+const tests = {
+    Test1: console.log(strictEquals(1, 1)),
+    Test2: strictEquals(NaN, NaN), // Exception
+    Test3: strictEquals(0, -0), // Exception
+    Test4: strictEquals(-0, 0), // Exception
+    Test5: strictEquals(1, "1"),
+    Test6: strictEquals(true, false),
+    Test7: strictEquals(false, false),
+    Test8: strictEquals("Water", "oil"),
+};
+
+console.table(tests);
